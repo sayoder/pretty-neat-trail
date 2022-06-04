b5bc639f7e569f3d6464c942f149dfdb290dcc65
Seth A. Yoder <seth.a.yoder@gmail.com>
2022-06-04 10:21:00 -0400

    UI work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59aafddb358a35e02f85c630ae51ee0f2e02cd543ff9df2d0011ff44f38a22bd
-size 1627
+oid sha256:9e10d3a4c6cbc66136b93f2d72b23d6e6fc4ff040948c33b14bcdfbf84175228
+size 1720
