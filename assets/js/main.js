b50138a6834e671fe890515a9484400617c66caa
Seth A. Yoder <seth.a.yoder@gmail.com>
2022-06-03 22:30:25 -0400

    Mobile improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82aed66a4e52ee5f510b480c2f8897b89623bd9f025942b1fd323a94918789e0
-size 1630
+oid sha256:59aafddb358a35e02f85c630ae51ee0f2e02cd543ff9df2d0011ff44f38a22bd
+size 1627
