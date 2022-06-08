3597254ec50c4868d2d7e85ff56eee834e6ad34c
Seth A. Yoder <seth.a.yoder@gmail.com>
2022-06-07 20:42:48 -0400

    Remove JS and SCSS files from LFS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,40 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:00aa5efba139f517b98d86ae81a5d88a11e8977e31c10ee371198da9d98c5962
-size 1327
+function doSlideIn() {
+    var extra = 60;
+    var bg = $(this).css('background-image');
+
+    // $(this).css({
+    //     background: "linear-gradient(0deg, rgba(28,28,28,60%) 0%, rgba(0,0,0,0) 38%)",
+    //     backgroundImage: bg,
+    //     backgroundSize: "cover",
+    //     backgroundRepeat: "no-repeat",
+    //     backgroundClip: "border-box",
+    //     backgroundPosition: "center center",
+    // });
+
+    var text = $(this).find('.category-text');
+    // if (!text.hasClass('animated')) {
+        var width = $(this).find('.category-slidein').width() + $(this).find('.category-slidein .category-name').width() + extra;
+        // text.dequeue().stop();
+        text.transition({ x: -width}, 300, 'easeOutCubic');
+    // }
+}
+
+function doSlideOut() {
+    var text = $(this).find('.category-text');
+    // text.addClass('animated');
+    text.transition({ x: 0}, 200, 'easeOutCubic', function() {
+        // $(this).removeClass('animated').dequeue();
+    });
+}
+
+$(document).ready(function() {
+    if (!window.matchMedia( "(hover: none)" ).matches) {
+        // We have hover capabilities
+        $('div.category-image').click(function(e) {
+            window.location = $(this).find('a.category-name').attr("href");
+            return false;
+        });
+    }
+
+    $('.category-image').hover(doSlideIn, doSlideOut);
+});
